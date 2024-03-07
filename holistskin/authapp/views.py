e04f925975a2b11f7d55829dfe1af6d28d8e13dc
Anthony Azubike Osigbe <thronesofglory@gmail.com>
2024-03-08 00:29:36 +0100

    backend user integration update

--- holistskin/authapp/views.py
+++ holistskin/authapp/views.py
@@ -1,7 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 
-# Create your views here.
+# Created views that links backend to front end for login authentications.
 def signin(request):
+  if request.method=="POST":
+    get_full_name=request.POST.get('full_name')
+    get_email=request.POST.get('email')
+    get_password=request.POST.get('pass1')
+    get_confirm_password=request.POST.get('pass2')
+
+    if get_password != get_confirm_password:
+      messages.info(request,'Password does not match')
+      return redirect('/auth/signin/')
+    
+    try:
+      if User.objects.get(username=get_email):
+        messages.warning(request,"Email Already Taken")
+        return redirect('/auth/signin/')
+      
+    except Exception as Identifier:
+      pass
+
+    myuser=User.objects.create_user(get_email,get_email,get_password)
+    myuser.save()
+    messages.success(request,'User Created Please Login')
+
+    return redirect('/auth/login/')
+
+  
+
   return render(request,'signin.html')
 
 def handleLogin(request):
