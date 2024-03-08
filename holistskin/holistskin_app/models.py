fbf6b354e9b6054b7e793103b9b87cb9d25d1b12
Anthony Azubike Osigbe <thronesofglory@gmail.com>
2024-03-08 12:41:32 +0100

    models.py and admin.py file update for contact section

--- holistskin/holistskin_app/models.py
+++ holistskin/holistskin_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+# This code defines models for managing contacts, allowing the
+# website to receive and store messages from site visitors.
+class Contact(models.Model):
+  name=models.CharField(max_length=25)
+  email=models.EmailField()
+  phonenumber=models.CharField(max_length=10)
+  subject=models.CharField(max_length=30)
+  message=models.TextField()
+
+
+
