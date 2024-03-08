6cc69d88828778b6022be551ef36749c57885c6a
Anthony Azubike Osigbe <thronesofglory@gmail.com>
2024-03-08 17:11:24 +0100

    Blog updates from Backend to front end Test complete

--- holistskin/holistskin_app/models.py
+++ holistskin/holistskin_app/models.py
@@ -19,7 +19,7 @@ class Blogs(models.Model):
   decription=models.TextField()
   authname=models.CharField(max_length=20)
   img=models.ImageField(upload_to='blog',blank=True,null=True)
-  timestamp=models.DateTimeField(auto_now_add=True,blank=True,null=True)
+  timeStamp=models.DateTimeField(auto_now_add=True,blank=True,null=True)
 
   def __str__(self):
     return self.title
