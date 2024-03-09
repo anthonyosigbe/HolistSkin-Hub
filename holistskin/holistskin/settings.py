5324db22486e140f7891a923da3791419c49436d
Anthony Azubike Osigbe <thronesofglory@gmail.com>
2024-03-09 21:40:49 +0100

    Update settings.py

--- holistskin/holistskin/settings.py
+++ holistskin/holistskin/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 from django.contrib.messages import constants as messages
 
@@ -136,3 +136,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MESSAGE_TAGS={
   messages.ERROR:'danger'
 }
+
+# Activate Django-Heroku
+django_heroku.settings(locals())
