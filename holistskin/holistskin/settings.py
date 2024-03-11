0cf222cee525a5ed969bf30556b8de24c13b15ce
braveking <114780375+braveking12@users.noreply.github.com>
2024-03-11 21:05:43 +0100

    update

--- holistskin/holistskin/settings.py
+++ holistskin/holistskin/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@%fcaxpu13uuptv%#7wp=#3i)uku2j%t4s2v1%fn=9p6=byr52
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
